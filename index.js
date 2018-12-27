4afe711fd4ca7d1d40c5d00cad428e59f7a4ca50
Gustos <gusto-s-s@hotmail.com>
2018-12-26 23:16:38 -0200

    RAML

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 const fastify = require('fastify');
-const server = fastify({logger: true});
-
-const schema = {
-    querystring: {
-        name: { type: 'string' }
-    }
-}
-
-server.get('/', async (req, res) => {
-    return { hello: `world ${req.query.name}`};
+const server = fastify({ logger: true });
+const mdns = require('mdns');
+const browser = mdns.createBrowser(mdns.tcp('http'), {
+  resolverSequence: [
+    mdns.rst.DNSServiceResolve(),
+    mdns.rst.getaddrinfo({ families: [4] })
+  ]
 });
 
-server.addHook('onSend', async (req, replay, payload) => {
-    let newPay = payload.replace(/}$/, ',"otp":"000000"}');
-    console.log(newPay);
-    return newPay;
+browser.on('serviceUp', service => {
+  console.log(service);
+});
+
+browser.on('serviceDown', service => {
+  console.log(service);
+});
+
+server.get('/', async (request, replay) => {
+  console.log(request.context)
+  return { hello: `world ${request.query.name}` };
+});
+
+server.addHook('onRequest', async (request, replay) => {
+  request.context = {
+    message: 'Olá!'
+  }
+  return request;
+});
+
+server.addHook('onSend', async (request, replay, payload) => {
+  let newPay = payload.replace(/^{/, '{"otp":"000000",');
+  console.log(newPay);
+  return newPay;
 });
 
 const start = async () => {
-    try {
-        await server.listen(8080);
-    } catch (err) {
-        process.exit(1);
-    }
+  try {
+    await server.listen(8088);
+  } catch (err) {
+    console.log('Server isn\'t running!')
+    process.exit(1);
+  }
+  try {
+    await browser.start();
+  } catch (err) {
+    console.log('MDNS isn\'t running!')
+    process.exit(1);
+  }
 }
 start();
---
+++ init.sh
@@ -0,0 +1,3 @@
+sudo apt install libavahi-compat-libdnssd-dev -y
+npm install
+npm main
